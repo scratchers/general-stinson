ba5abc59c71995e5e12621594aff03f0225142ee
Jeff Puckett <jeff@jeffpuckett.com>
2017-07-08 20:02:20 +0000

    test can parse statements for salutations [ci skip]

--- src/Parser.php
+++ src/Parser.php
@@ -4,5 +4,12 @@ namespace scratchers\salute;
 
 class Parser
 {
-
+    /**
+     * @return mixed
+     *   substring salutation from statement or FALSE on failure
+     */
+    public function parse(string $statement)
+    {
+        return false;
+    }
 }
--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -9,4 +9,60 @@ class ParserTest extends TestCase
     {
         $this->assertInstanceOf(Parser::class, new Parser);
     }
+
+    public function saluteDataProvider()
+    {
+        return [
+            [
+                "This is gonna be a major cleanup.",
+                "major cleanup",
+            ],
+            [
+                "Only women with major baggage make porn.",
+                "major baggage",
+            ],
+            [
+                "I've got a major craving for a mojito.",
+                "major craving",
+            ],
+            [
+                "It's been a major pleasure.",
+                "major pleasure",
+            ],
+            [
+                "It's really a major buzz-kill.",
+                "major buzz-kill",
+            ],
+            [
+                "Look, it's a private thing between me and Ted.",
+                "private thing",
+            ],
+            [
+                "Oh, God, we're back to your stupid little private joke again?",
+                "private joke",
+            ],
+            [
+                "Sorry everything is in general disarray.",
+                "general disarray",
+            ],
+            [
+                "It's general knowledge.",
+                "general knowledge",
+            ],
+            [
+                "I've got a kernel stuck in my teeth.",
+                "kernel stuck",
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider saluteDataProvider
+     */
+    public function test_returns_salutation($statement, $expected)
+    {
+        $actual = (new Parser)->parse($statement);
+
+        $this->assertSame($expected, $actual);
+    }
 }
